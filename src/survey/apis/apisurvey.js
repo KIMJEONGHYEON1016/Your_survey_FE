018a50d87936cd010ebcc0817bd0c8bfa496961d
DESKTOP-KMNUJB9\dkemfdk <dkemfdk4@Naver.com>
2025-05-25 18:37:50 +0900

    api2

--- src/survey/apis/apisurvey.js
+++ src/survey/apis/apisurvey.js
@@ -1,14 +1,14 @@
 import requestData from '@/survey/libs/requestData';
 
-export const createSurvey = (data) => requestData('/api2/survey', 'POST', data);
+export const createSurvey = (data) => requestData('/api/survey', 'POST', data);
 
 // 설문 정보 불러오기
 export const getSurveyByToken = (token) =>
-  requestData(`/api2/survey/respond/${token}`);
+  requestData(`/api/survey/respond/${token}`);
 
 // 설문 응답 제출
 export const submitSurveyAnswer = (token, data) =>
-  requestData(`/api2/survey/respond/${token}`, 'POST', data);
+  requestData(`/api/survey/respond/${token}`, 'POST', data);
 
 export const getSurveyResult = (ownerToken) =>
-  requestData(`/api2/survey/manage/${ownerToken}`);
+  requestData(`/api/survey/manage/${ownerToken}`);
