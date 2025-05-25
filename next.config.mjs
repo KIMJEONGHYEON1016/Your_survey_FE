016018a254df51e16f043b3e5ebe675a07b9d2ae
DESKTOP-KMNUJB9\dkemfdk <dkemfdk4@Naver.com>
2025-05-25 12:01:33 +0900

    주소변경

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/api2/:path*',
-        destination: 'http://52.79.118.248:8080/api/survey/:path*', // EC2 백엔드 주소 하드코딩
-      },
-    ];
-  },
-};
-
-export default nextConfig;
+    reactStrictMode: true,
+    compiler: {
+      styledComponents: true,
+    },
+    async rewrites() {
+      return [
+        {
+          source: '/api2/:path*',
+          destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`, // → http://localhost:8080/api/survey
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
