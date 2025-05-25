8fdb80ddc2df066f88f4ed8fb2b30f84cfc0ef0e
DESKTOP-KMNUJB9\dkemfdk <dkemfdk4@Naver.com>
2025-05-25 15:39:04 +0900

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    compiler: {
-      styledComponents: true,
-    },
-    async rewrites() {
-      return [
-        {
-          source: '/api2/:path*',
-          destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`, // → http://localhost:8080/api/survey
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
-  
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api2/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`, // → http://localhost:8080/api/survey
+      },
+    ];
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/survey/create',
+        permanent: false, // 301으로 할 수도 있음
+      },
+    ];
+  },
+};
+
+export default nextConfig;
