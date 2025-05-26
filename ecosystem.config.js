cf4cd89586316b7d50375193cf8f137f442df901
DESKTOP-KMNUJB9\dkemfdk <dkemfdk4@Naver.com>
2025-05-27 03:19:36 +0900

    .

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,14 +3,14 @@ module.exports = {
     {
       name: 'your-survey-frontend',
       script: 'npm',
-      args: 'run dev',
+      args: 'run start',
       cwd: '/var/lib/jenkins/workspace/survey_frontend',
       interpreter: 'none',
       env: {
         NODE_ENV: 'production',
-        PORT: 3000
         // NEXT_PUBLIC_API_URL은 .env.production에서 관리
       }
     }
   ]
 };
+np
